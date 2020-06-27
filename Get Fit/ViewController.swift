a70e18cb956c6ca75c8db9ef79d6f45dd0cdc55d
Ewin Zuo <zuoewin@gmail.com>
2020-06-27 15:55:58 -0500

    Initial Commit

---
+++ Get Fit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get Fit
+//
+//  Created by Ewin Zuo on 6/27/20.
+//  Copyright © 2020 Ewin Zuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
