122cecbc85dbdb0918ef60b59b3095db1afac4c0
Ewin Zuo <zuoewin@gmail.com>
2020-06-27 20:28:52 -0500

    added basic storyboard

--- Get Fit/ViewController.swift
+++ Get Fit/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cardView.roundCorners(cornerRadius: 10.0)
     }
 
 
 }
-
+extension UIView {
+    func roundCorners(cornerRadius: Double) {
+        self.layer.cornerRadius = CGFloat(cornerRadius)
+//        self.layer.shadowColor = UIColor.darkGray.cgColor
+//        self.layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
+//        self.layer.shadowRadius = 0
+//        self.layer.shadowOpacity = 0.5
+        self.layer.borderWidth = 3
+        self.layer.borderColor = UIColor(red: 0.96, green: 0.56, blue: 0.56, alpha: 1.00).cgColor
+    }
+    
+}
