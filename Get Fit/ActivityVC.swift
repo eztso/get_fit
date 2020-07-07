0290e91da4dca57e2676a239748b84ae1da9b9ca
Ewin Zuo <zuoewin@gmail.com>
2020-07-07 18:40:39 -0500

    stuff

---
+++ Get Fit/ActivityVC.swift
@@ -0,0 +1,37 @@
+//
+//  ActivityVC.swift
+//  Get Fit
+//
+//  Created by Ewin Zuo on 7/5/20.
+//  Copyright © 2020 Ewin Zuo. All rights reserved.
+//
+
+import UIKit
+
+class ActivityVC: UIViewController {
+
+    @IBOutlet weak var stepsOutlet: UIButton!
+    @IBOutlet weak var milesOutlet: UIButton!
+
+    @IBAction func onStepsPressed(_ sender: Any) {
+    }
+    @IBAction func onMilesPressed(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
